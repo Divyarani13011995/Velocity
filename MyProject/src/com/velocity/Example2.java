ef3338efe07f088642a9a6f2fa11f38471a8ab92
divya <divyaranikarande1@gmail.com>
2022-12-21 16:08:49 +0530

    second version arraylist created

---
+++ MyProject/src/com/velocity/Example2.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+
+    ArrayList<String> al = new ArrayList <String>();
+    al.add("divya");
+    al.add("puja");
+    System.out.println(al);
+
+	}
+
+}

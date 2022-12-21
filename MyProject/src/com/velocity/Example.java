e1a2110b2b5de431572b395f662ab7a4ff248b36
divya <divyaranikarande1@gmail.com>
2022-12-21 15:51:33 +0530

    Initial Commit

---
+++ MyProject/src/com/velocity/Example.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Example {
+
+	public static void main(String[] args) {
+		ArrayList <Integer> al = new ArrayList <Integer> ();
+		al.add(10);
+		al.add(20);
+		System.out.println(al);
+
+	}
+
+}
